9c0dfcff5a5530deb3c75f8015d255a443cbdd25
Connor Leech <connorleech@gmail.com>
2014-11-09 18:13:45 -0800

    blinky

---
+++ js/lib/blink.js
@@ -0,0 +1,10 @@
+var blinks = document.querySelectorAll('blink');
+var visible = true;
+setInterval(function () {
+    visible = !visible;
+    for (var i = 0; i < blinks.length; i++) {
+        blinks[i].style.visibility = visible ? 'visible' : 'hidden';
+    }
+}, 500);
+
+blinks.style.color = "blue";

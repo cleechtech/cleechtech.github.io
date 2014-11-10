026038587e9f4190f40858a3185b9a2e2eaf2a79
Connor Leech <connorleech@gmail.com>
2014-11-09 19:05:13 -0800

    rework

--- js/lib/blink.js
+++ js/lib/blink.js
@@ -1,13 +1,16 @@
 var blinks = document.querySelectorAll('blink');
 var visible = true;
+
+var anchor = blinks[0].children[0];
+anchor.style.color = "#000080";
+anchor.style.fontSize = '32px';
+
 setInterval(function () {
     visible = !visible;
     for (var i = 0; i < blinks.length; i++) {
         blinks[i].style.visibility = visible ? 'visible' : 'hidden';
     }
 }, 500);
-
-blinks.style.color = "blue";
 
 // senor javascript developer
 
